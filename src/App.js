64964e75234d82125271127bf65db1554d0776ad
Athiravinod0612 <athiraponnempara0612@gmail.com>
2024-01-03 17:30:52 +0530

    Personal component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import React from 'react';
+import { Container, Grid, Switch } from '@mui/material';
 
-import './App.css';
+import Portfolio from './Pages/Portfolio/Portfolio'
+import Header from './Components/Header/Header';
+import Resume from './Pages/Resume/Resume';
+import Footer from './Components/Footer/Footer';
+
+import { Route, Router } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={4} style={{ backgroundColor: "red" }}>
+          444
+
+        </Grid>
+
+        <Grid item xs={8} style={{ backgroundColor: "blue" }}>
+          <Header />
+
+          <Router>
+            <Switch>
+              <Route path='/Portfolio'><Portfolio /></Route>
+              <Route path='/'><Resume /></Route>
+
+            </Switch>
+          </Router>
 
 
-    </div>
+
+
+
+          <Footer />
+        </Grid>
+
+
+      </Grid>
+    </Container>
   );
 }
 
